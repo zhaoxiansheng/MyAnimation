89ea6f0c222ce087fcb90e11600432b21fc79438
p_rainyzhao <p_rainyzhao@tencent.com>
2020-11-11 11:51:14 +0800

    测试通知

--- app/src/main/java/com/example/zy/myanimation/service/BookManagerService.java
+++ app/src/main/java/com/example/zy/myanimation/service/BookManagerService.java
@@ -1,11 +1,19 @@
 package com.example.zy.myanimation.service;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Binder;
+import android.os.Build;
 import android.os.IBinder;
 import android.os.RemoteException;
 
+import com.example.zy.myanimation.R;
+import com.example.zy.myanimation.activity.MainActivity;
 import com.example.zy.myanimation.aidl.Book;
 import com.example.zy.myanimation.aidl.IBookManager;
 import com.example.zy.myanimation.aidl.IOnNewBookArrivedListener;
@@ -16,6 +24,8 @@ import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
 
 /**
  * Created on 2017/12/4.
@@ -83,6 +93,9 @@ public class BookManagerService extends Service {
         mBookList.add(new Book(1, "Android"));
         mBookList.add(new Book(2, "ios"));
         new Thread(new ServiceWorker()).start();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotification("MyService", "MyName");
+        }
     }
 
     private void onNewBookArrived(Book book) throws RemoteException {
@@ -112,5 +125,43 @@ public class BookManagerService extends Service {
                 }
             }
         }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void createNotification(String channelID, String channelName) {
+        NotificationChannel channel = new NotificationChannel(channelID, channelName, NotificationManager.IMPORTANCE_NONE);
+        channel.setLightColor(Color.BLUE);
+        channel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+        // 设置渠道描述
+        channel.setDescription("测试通知组");
+        // 设置绕过请勿打扰模式
+        channel.setBypassDnd(true);
+        // 桌面Launcher的消息角标
+        channel.canShowBadge();
+        // 设置显示桌面Launcher的消息角标
+        channel.setShowBadge(true);
+        // 设置通知出现时声音，默认通知是有声音的
+        channel.setSound(null, null);
+        // 设置通知出现时的闪灯（如果 android 设备支持的话）
+        channel.enableLights(true);
+        // 设置通知出现时的震动（如果 android 设备支持的话）
+        channel.enableVibration(true);
+        channel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400,
+                300, 200, 400});
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 11,
+                new Intent(getApplicationContext(), MainActivity.class), 0);
+
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        manager.createNotificationChannel(channel);
+        Notification notification = new NotificationCompat.Builder(this, channelID)
+                .setContentTitle("12312312312313")
+                .setContentText("322323223223")
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentIntent(pendingIntent)
+//                .setChannelId(CALENDAR_ID)
+                .build();
+
+        startForeground(1, notification);
     }
 }
